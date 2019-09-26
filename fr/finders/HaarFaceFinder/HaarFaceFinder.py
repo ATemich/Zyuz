1750de0162f5c349c74ea8b0fca8c51efcbc1452
povle <pasha@blinov.co>
2019-09-26 22:05:31 +0300

    default args for haar

--- fr/finders/HaarFaceFinder/HaarFaceFinder.py
+++ fr/finders/HaarFaceFinder/HaarFaceFinder.py
@@ -4,14 +4,17 @@ import cv2
 import numpy
 
 class HaarFaceFinder(FaceFinder):
-    def __init__(self, path):
+    def __init__(self, path, **kwargs):
         super().__init__()
         self.path = path
         self.cascade = cv2.CascadeClassifier(path)
+        self.find_kwargs = kwargs
 
     def find(self, pic: numpy.ndarray, *args, **kwargs):
         gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
         matches = []
-        for x, y, w, h in self.cascade.detectMultiScale(gray, *args, **kwargs):
+        kw = self.find_kwargs.copy()
+        kw.update(kwargs)
+        for x, y, w, h in self.cascade.detectMultiScale(gray, *args, **kw):
             matches.append(Face(None, (x, y), (w, h), None))
         return matches
