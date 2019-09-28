649a9175cf5eddb378fc2678ad90516d39adf9f2
povle <pasha@blinov.co>
2019-09-28 18:50:23 +0300

    typing

--- fr/recognizers/FaceRecognizer.py
+++ fr/recognizers/FaceRecognizer.py
@@ -11,5 +11,5 @@ class FaceRecognizer:
     def load_from_encodings(self, path):
         raise NotImplementedError
 
-    def recognize(self, frame: numpy.ndarray, face: Face = None) -> str:
+    def recognize(self, frame: numpy.ndarray, face: Face = None):
         raise NotImplementedError
