2dc279a868364c59ce56f676e5c86b3ef5b8b711
povle <pasha@blinov.co>
2019-10-06 19:17:26 +0300

    update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from fr.finders import HaarFaceFinder
 from fr.finders.HaarFaceFinder.cascades import frontalface
 from fr.recognizers import FaceNet
+from fr.distance_estimators import dlibEstimator
 import cv2
 import dlib
 import time
@@ -11,8 +12,9 @@ vc = cv2.VideoCapture('video4.mp4')
 
 MULTIPLIER = 1
 finder = HaarFaceFinder(frontalface, scaleFactor=1.3, minNeighbors=5, maxSize=(120, 120))
-recognizer = FaceNet(threshold=0.53)
+recognizer = FaceNet(threshold=0.55)
 recognizer.load_from_images('images')
+estimator = dlibEstimator()
 frame_counter = 0
 
 faces = []
@@ -24,14 +26,14 @@ while vc.isOpened():
     #frame = cv2.resize(frame, (int(frame.shape[1] * MULTIPLIER), int(frame.shape[0] * MULTIPLIER)))
 
     if frame_counter % 15 == 0:
-        new_faces = finder.find(frame, padding=30)
+        new_faces = finder.find(frame)
         for new_face in new_faces:
             for face in faces:
                 if new_face in face or face in new_face:
                     break
             else:
-                faces.append(new_face)
-                face = new_face
+                face = new_face.padded(1.1)
+                faces.append(face)
                 face.tracker = dlib.correlation_tracker()
                 rect = dlib.rectangle(*face.start, *face.end)
                 face.tracker.start_track(frame, rect)
@@ -41,11 +43,16 @@ while vc.isOpened():
     img = frame
     for face in faces:
         img = cv2.rectangle(img, face.start, face.end, (255, 0, 0), 2)
+        for eye in estimator.find_eyes(frame, face):
+            cv2.circle(img, (int(eye.x), int(eye.y)), 2, (255, 0, 0), -1)
         if face.id is None:
             cv2.putText(img, 'Recognizing...', tuple(face.start), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
             face.get_recognized(frame, recognizer)
         else:
-            cv2.putText(img, face.id, tuple(face.start), cv2.FONT_HERSHEY_DUPLEX, 1, 255)
+            dist = round(estimator.estimate(frame, face.padded(0.85)), 2)
+            text = f'{face.id}-{dist}'
+            color = (0, 0, 255) if dist >= 20 else (255, 0, 0)
+            cv2.putText(img, text, tuple(face.start), cv2.FONT_HERSHEY_DUPLEX, 1, color)
 
     key = cv2.waitKey(1)
     cv2.imshow("preview", img)
@@ -59,5 +66,5 @@ while vc.isOpened():
     times.append(t)
     if len(times) > 10:
         times = times[1:]
-    print(len(times)/sum(times))
+    #print(len(times)/sum(times))
 cv2.destroyWindow("preview")
